024620c3e6ca992fa0ce64f1fc4003a020f648ac
Santoshi-zalte <105422523+Santoshi-zalte@users.noreply.github.com>
2022-05-12 14:41:47 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,57 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
+<h2><b>ABOUT US</b></h2>
+<h3>Developed BY SNJB'S COLLEGE OF ENGINEERING STUDENT,THIS NOTICE BOARD WILL SHOW RECENT UPDATED NOTICE</h3>
+<div class="card">
+
+<!-- replace image here -->
+  <img src="/images/person.jpg" alt="santoshi" style="width:100%">
+
+  <h1>Santoshi Zalte</h1>
+  <p class="title">TE COMP</p>
+  <p>Pune University</p>
+  <div style="margin: 24px 0;">
+  <!-- put links on # now no need -->
+    <a href="#"><i class="fa fa-dribbble"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
+  </div>
+  <p><button>Contact</button></p>
+</div>
